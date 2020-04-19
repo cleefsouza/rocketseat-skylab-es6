8c9f1d2e049776b7ccb467e08589f0540a4bccdc
Cleef Souza <a.cleef.souza@gmail.com>
2020-04-18 23:02:45 -0300

    Arquivos e suas configuracoes

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
-class Teste {
-    metodo() {
-
+class List {
+    constructor() {
+        this.data = [];
     }
 
-    outro() {
-
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List {
+    constructor() {
+        super();
+
+        this.usuario = 'Cleef Souza';
+    }
+
+    soma(a, b) {
+        return a + b;
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('newTodo')
+    .onclick = () => {
+        MinhaLista.add('New todo');
+
+        let resultado = MinhaLista.soma(5, 2);
+        console.log(`Resultado: ${resultado}`);
+    }

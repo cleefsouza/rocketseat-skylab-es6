1ec42d513988627475312f6aff93671c0704beff
Cleef Souza <a.cleef.souza@gmail.com>
2020-04-19 20:02:57 -0300

    Desafio 2, Modulo 3

--- es6_webpack/src/main.js
+++ es6_webpack/src/main.js
@@ -1,5 +1,6 @@
 /**
- * Exe 1
+ * Desafio 1
+ * Exercício
  */
 
 import Usuario, { Usuario as ClasseUsuario, idade as IdadeUsuario} from "./desafio_1/functions";
@@ -47,3 +48,55 @@ class Api {
 }
 
 Api.getUserInfo('cleefsouza');
+
+/**
+ * Desafio 2
+ * Exercício
+ */
+
+// Funão delay aciona o .then após 1s
+const delay = () => new Promise(
+    resolve => setTimeout(resolve, 1000)
+);
+
+async function umPorSegundo() {
+    await delay();
+    console.log('1s');
+
+    await delay();
+    console.log('2s');
+
+    await delay();
+    console.log('3s');
+}
+
+umPorSegundo();
+
+async function getUserFromGithub(user) {
+    try {
+        const response = await axios.get(
+            `http://api.github.com/users/${user}`
+        );
+
+        console.log(response.data);
+    } catch (err) {
+        console.warn('Erro aos buscar usuário!');
+    }
+}
+
+getUserFromGithub('diego3g');
+getUserFromGithub('diego3g124123');
+
+class Github {
+    static async getRepositories(repo) {
+        try {
+            const response = await axios.get(`https://api.github.com/repos/${repo}`);
+            console.log(response.data)
+        } catch (err) {
+            console.warn('Respositório não encontrado')
+        }
+    }
+}
+
+Github.getRepositories('cleefsouza/rocketseat-skylab-es6');
+Github.getRepositories('cleefsouza/este-nao-existe');
